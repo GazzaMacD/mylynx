39822c7b05d48c68c8bc7d236003f51e3ab30610
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-11-11 11:06:56 +0200

    Create custom user model, no tests yet

--- mylinx_backend/config/settings/base.py
+++ mylinx_backend/config/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -99,3 +100,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CustomUser model
+AUTH_USER_MODEL = "users.CustomUser"
---
+++ mylinx_backend/users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.db import models
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # Email is already required by USERNAME_FIELD
+
+    def __str__(self):
+        return self.email
